a9ec952546a88e618ff09aa3ee545eaab5939ec0
alisher <abdirimov110@gmail.com>
2024-04-08 00:44:32 +0500

    remove login

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import "./assets/style/main.scss";
-import UserProvider from "./context/userContext";
 import Root from "./components/root";
+import UserProvider from "./context/userContext";
+import "./assets/style/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
